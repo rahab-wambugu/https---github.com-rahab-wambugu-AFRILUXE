fb4fd2eefa316d17266f483f4c280332894168f8
rahab-wambugu <wambugurahab53@gmail.com>
2025-05-09 02:57:15 +0300

    wow

---
+++ index.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('.menu-btn');
+let navbar = document.querySelector('.header .navbar');
+let search = document.querySelector('#search-btn');
+let searchForm = document.querySelector('.search-form');
+let closeSearch = document.querySelector('#close'); 
+let cart = document.querySelector('#cart-btn');
+let cartItems = document.querySelector('.cart-items-container');
+let closeCart = document.querySelector('#close-cart');
