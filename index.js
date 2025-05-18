39cb21bfe2b443d064fd6742ff1c1cd7b1f29a57
rahab-wambugu <wambugurahab53@gmail.com>
2025-05-19 01:17:16 +0300

    updated navbar to be responsive

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let menu = document.querySelector('.menu-btn');
-let navbar = document.querySelector('.header .navbar');
-let search = document.querySelector('#search-btn');
-let searchForm = document.querySelector('.search-form');
-let closeSearch = document.querySelector('#close'); 
-let cart = document.querySelector('#cart-btn');
-let cartItems = document.querySelector('.cart-items-container');
-let closeCart = document.querySelector('#close-cart');
+
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display ='flex'
+}
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display ='none'
+}
