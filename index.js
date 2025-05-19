e8cf88950b844cd979163b615834f253750eb8ff
rahab-wambugu <wambugurahab53@gmail.com>
2025-05-20 02:07:17 +0300

    contact and footer done

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display ='none'
 }
+
+function toggleAccordion(index) {
+    const content = document.getElementById(`content-${index}`);
+    const icon = document.getElementById(`icon-${index}`);
+    const minusI =`<i class="fa-solid fa-minus"></i>`;
+    const plusI =`<i class="fa-solid fa-plus"></i>`;
+
+if (content.style.maxHeight && content.style.maxHeight !== '0px') {
+      content.style.maxHeight = '0';
+      icon.innerHTML = plusI;
+} else {
+      content.style.maxHeight = content.scrollHeight + 'px';
+      icon.innerHTML = minusI;
+    }
+}
